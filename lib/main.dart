cb43965ef6a8eb7f84b724f042fe3671b7983556
elnur <elnurrabbimov@gmail.com>
2023-03-23 16:56:49 +0500

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Elnur',    
+      title: 'Elnur',         
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(),
+      home: HomeScreen(), 
       routes: {
         '/home': (context) => HomeScreen(),
         '/numbers': (context) => NumbersScreen(),
